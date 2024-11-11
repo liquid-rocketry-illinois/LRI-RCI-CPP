18cf6db38a4637bc13972ee4a12fe42d0af688c2
Jacob Baumel <jacob.a.baumel@gmail.com>
2024-11-11 16:35:42 -0600

    Threaded COMPort interface to allow for determining if data is available without reading from port

--- include/interfaces/COMPort.h
+++ include/interfaces/COMPort.h
@@ -3,18 +3,47 @@
 
 #include <string>
 #include <Windows.h>
+#include <atomic>
+#include <thread>
+#include <mutex>
 #include "RCP_Interface.h"
 
 namespace LRI::RCI {
     class COMPort : public RCP_Interface {
+        class RingBuffer;
+        static constexpr int bufferSize = 1024;
+
         char* portname;
         HANDLE port;
         bool open = false;
-        DWORD lastErrorVal;
+        std::atomic_ulong lastErrorVal;
+
+        RingBuffer* buffer;
+        std::atomic_bool read;
+        std::thread* thread;
+        mutable std::mutex dataAccess;
+
+        void threadRead();
+
+
+        class RingBuffer {
+            int buffersize;
+            int datastart;
+            int dataend;
+            uint8_t* data;
+
+        public:
+            explicit RingBuffer(int buffersize);
+            ~RingBuffer();
+
+            int size();
+            uint8_t pop();
+            void push(uint8_t);
+        };
 
     public:
         explicit COMPort(const char* portname, DWORD baudrate);
-        ~COMPort();
+        ~COMPort() override;
         bool close();
         bool isOpen() const override;
         bool hasData() const override;
--- src/interfaces/COMPort.cpp
+++ src/interfaces/COMPort.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 namespace LRI::RCI {
-    COMPort::COMPort(const char* _portname, DWORD baudrate) {
+    COMPort::COMPort(const char* _portname, DWORD baudrate) : dataAccess() {
         portname = new char[strlen(_portname)];
         strcpy(portname, _portname);
         port = CreateFile(portname, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
@@ -30,8 +30,24 @@ namespace LRI::RCI {
             return;
         }
 
+        COMMTIMEOUTS timeouts = {0};
+        timeouts.ReadIntervalTimeout = 50;
+        timeouts.ReadTotalTimeoutConstant = 50;
+        timeouts.ReadTotalTimeoutMultiplier = 10;
+        timeouts.WriteTotalTimeoutConstant = 50;
+        timeouts.WriteTotalTimeoutMultiplier = 10;
+
+        if(!SetCommTimeouts(port, &timeouts)) {
+            lastErrorVal = GetLastError();
+            return;
+        }
+
         open = true;
         lastErrorVal = 0;
+
+        buffer = new RingBuffer(bufferSize);
+        read = true;
+        thread = new std::thread(this->threadRead);
     }
 
     COMPort::~COMPort() {
@@ -40,6 +56,12 @@ namespace LRI::RCI {
 
     bool COMPort::close() {
         open = false;
+        read = false;
+        thread->join();
+        delete thread;
+        thread = nullptr;
+        delete buffer;
+        buffer = nullptr;
         return !CloseHandle(port);
     }
 
@@ -54,19 +76,76 @@ namespace LRI::RCI {
     size_t COMPort::sendData(const void* bytes, size_t length) const {
         DWORD bytes_written = 0;
 
+        dataAccess.lock();
         if(!WriteFile(port, bytes, length, &bytes_written, nullptr)) return -1;
+        dataAccess.unlock();
         return bytes_written;
     }
 
-    size_t COMPort::readData(void* bytes, size_t bufferlength) const {
-        DWORD bytes_read = 0;
+    size_t COMPort::readData(void* rawbytes, size_t bufferlength) const {
+        int bytesread;
+        uint8_t* bytes = (uint8_t*) rawbytes;
 
-        if(!ReadFile(port, bytes, bufferlength, &bytes_read, nullptr)) return -1;
-        return bytes_read;
+        dataAccess.lock();
+        for(bytesread = 0; buffer->size() > 0 && bytesread < bufferlength; bytesread++) {
+            bytes[bytesread] = buffer->pop();
+        }
+        dataAccess.unlock();
+
+        return bytesread + 1;
     }
 
     std::string COMPort::interfaceType() const {
         return std::string("Serial Port (") + portname + ")";
     }
 
+    bool COMPort::hasData() const {
+        dataAccess.lock();
+        bool hasData = buffer->size() > 0;
+        dataAccess.unlock();
+        return hasData;
+    }
+
+    void COMPort::threadRead() {
+        while(readData) {
+            dataAccess.lock();
+            if(buffer->size() < bufferSize) continue;
+            uint8_t byte;
+            DWORD read;
+
+            if(!ReadFile(port, &byte, 1, &read, nullptr) || read != 1) {
+                lastErrorVal = GetLastError();
+                continue;
+            }
+
+            buffer->push(byte);
+            dataAccess.unlock();
+        }
+    }
+
+    COMPort::RingBuffer::RingBuffer(int _buffersize) :
+        buffersize(_buffersize), datastart(0), dataend(0), data(nullptr) {
+        data = new uint8_t[buffersize];
+        memcpy(data, 0, buffersize);
+    }
+
+    COMPort::RingBuffer::~RingBuffer() {
+        delete[] data;
+    }
+
+    int COMPort::RingBuffer::size() {
+        return dataend - datastart;
+    }
+
+    uint8_t COMPort::RingBuffer::pop() {
+        if(size() == 0) return 0;
+        uint8_t retval = data[datastart % buffersize];
+        datastart++;
+        return retval;
+    }
+
+    void COMPort::RingBuffer::push(uint8_t value) {
+        data[dataend] = value;
+        dataend++;
+    }
 }
