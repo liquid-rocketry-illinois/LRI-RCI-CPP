7ac62a299615d8a3fe1b9c5d818e0a9f8d02df2d
Jacob Baumel <jacob.a.baumel@gmail.com>
2025-03-17 07:26:51 -0500

    estop stuff

---
+++ include/hardware/EStop.h
@@ -0,0 +1,19 @@
+#ifndef ESTOP_H
+#define ESTOP_H
+
+namespace LRI::RCI {
+    class EStop {
+        bool isStopped;
+
+        EStop() = default;
+        ~EStop() = default;
+    public:
+        static EStop* getInstance();
+
+        void ESTOP();
+        [[nodiscard]] bool isEstopped() const;
+        void receiveRCPUpdate(bool isStopped);
+    };
+}
+
+#endif //ESTOP_H
---
+++ src/hardware/EStop.cpp
@@ -0,0 +1,23 @@
+#include "hardware/EStop.h"
+#include "RCP_Host/RCP_Host.h"
+
+namespace LRI::RCI {
+    EStop* EStop::getInstance() {
+        static EStop* instance = nullptr;
+        if(instance == nullptr) instance = new EStop();
+        return instance;
+    }
+
+    void EStop::ESTOP() {
+        RCP_sendEStop();
+        isStopped = true;
+    }
+
+    bool EStop::isEstopped() const {
+        return isStopped;
+    }
+
+    void EStop::receiveRCPUpdate(bool _isEStopped) {
+        isStopped = _isEStopped;
+    }
+}
