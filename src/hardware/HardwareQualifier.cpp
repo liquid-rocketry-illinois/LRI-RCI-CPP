34b147520c35d997d83dc5ad94a50f819e4ba81f
Jacob Baumel <jacob.a.baumel@gmail.com>
2025-10-30 16:42:54 -0500

    Changed serialized format of hardware qualifier to print devclass in hex

--- src/hardware/HardwareQualifier.cpp
+++ src/hardware/HardwareQualifier.cpp
@@ -1,5 +1,7 @@
 #include "hardware/HardwareQualifier.h"
 
+#include <format>
+
 namespace LRI::RCI {
     bool HardwareQualifier::operator<(HardwareQualifier const& rhf) const {
         if(devclass == rhf.devclass) return id < rhf.id;
@@ -8,6 +10,6 @@ namespace LRI::RCI {
 
     // Used in imgui IDs, not necessarily meant for display. In the format of DEVCLASS-ID-NAME
     std::string HardwareQualifier::asString() const {
-        return std::to_string(devclass) + "-" + std::to_string(id) + "-" + name;
+        return std::format("0x{:2X}-{}-{}", static_cast<uint8_t>(devclass), id, name);
     }
 } // namespace LRI::RCI
